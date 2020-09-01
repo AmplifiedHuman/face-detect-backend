661e72acdaa7e0095e543521c7ffbf316c8ff5ec
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-09-01 14:33:30 +0800

    added local dev switch

--- db/db.js
+++ db/db.js
@@ -1,19 +1,34 @@
 const knex = require('knex');
 
+const localDB = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+};
+
 const DBConnection = () => {
   let db = null;
   let instance = 0;
 
   function DBConnect() {
     try {
-      const db = knex({
-        client: 'pg',
-        connection: {
-          connectionString: process.env.DATABASE_URL,
-          ssl: { rejectUnauthorized: false },
-        },
-      });
-      return db;
+      // use local server if not in prod
+      if (!process.env.DATABASE_URL) {
+        const db = knex({ client: 'pg', connection: localDB });
+        console.log('Starting connection to local db');
+        return db;
+      } else {
+        console.log('Starting connection to remote db');
+        const db = knex({
+          client: 'pg',
+          connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false },
+          },
+        });
+        return db;
+      }
     } catch (e) {
       console.log(e);
       return e;
